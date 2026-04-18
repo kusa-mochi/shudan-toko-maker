7cb9e4df66d783db77c2f0b639d3c0117557afb3
Kusa Mochi <whoatemyapplepie@gmail.com>
2026-04-18 22:49:10 +0900

    named volumeの削除を含むcompose down用スクリプト追加定義

--- down.cmd
+++ down.cmd
@@ -1,7 +1,3 @@
-@REM docker compose -f docker-compose.init.yml down -v
-@REM docker compose -f docker-compose.dev.yml down -v
-@REM docker compose down -v
-
 docker compose -f docker-compose.init.yml down
 docker compose -f docker-compose.dev.yml down
 docker compose down
